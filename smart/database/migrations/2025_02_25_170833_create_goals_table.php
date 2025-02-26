f46011e191f6895d0f0fd0705775d26a2fef800b
latifi777 <abdelalilatifi3@gmail.com>
2025-02-26 15:19:25 +0000

    adding migrations

--- smart/database/migrations/2025_02_25_170833_create_goals_table.php
+++ smart/database/migrations/2025_02_25_170833_create_goals_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('goals', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->foreignId('family_id')->nullable()->constrained('families')->onDelete('set null');
-            $table->rememberToken();
+            $table->decimal('target_amount', 10, 2);
+            $table->decimal('current_amount', 10, 2)->default(0);
+            $table->date('target_date')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('goals');
     }
 };
